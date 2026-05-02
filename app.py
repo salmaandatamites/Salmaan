ee9859f31585ac5a1b33b909d321c699633ed1a1
salmaandatamites <salmaandatamites@gmail.com>
2026-05-02 11:59:44 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeRegressor
 import joblib
 
 # --- Load Data --------------------------------------------------------
